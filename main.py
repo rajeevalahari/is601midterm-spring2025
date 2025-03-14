e160c55577b3ff204c8500b919e5ddd7c5758cff
Rajeev Kumar Alahari <ra833@njit.edu>
2025-03-14 00:47:51 -0400

    instantiated app

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")
+# main.py
+from app import App    
+
+# You must put this in your main.py because this forces the program to start when you run it from the command line.
+if __name__ == "__main__":
+    app = App().start()  # Instantiate an instance of App
