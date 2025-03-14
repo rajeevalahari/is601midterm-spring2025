46510ef507b3fdf60d55a0a15d08972dc31d86e3
Rajeev Kumar Alahari <ra833@njit.edu>
2025-03-14 17:23:08 -0400

    added history options

--- app/plugins/menu/__init__.py
+++ app/plugins/menu/__init__.py
@@ -5,9 +5,12 @@ class MenuCommand(Command):
     def execute(self):
         logging.info("MenuCommand: Displaying available commands")
         print("Available commands:")
-        print("  add   -> Adds two numbers")
-        print("  sub   -> Subtracts two numbers")
-        print("  mul   -> Multiplies two numbers")
-        print("  div   -> Divides two numbers")
+        print("  addition   -> Adds two numbers")
+        print("  subtraction   -> Subtracts two numbers")
+        print("  multplication   -> Multiplies two numbers")
+        print("  division   -> Divides two numbers")
         print("  exit  -> Exits the application")
         print("  menu  -> Displays this menu")
+        print("  showhistory  -> Shows history(last 5 calculations)")
+        print("  clearhistory  -> Clears history")
+
